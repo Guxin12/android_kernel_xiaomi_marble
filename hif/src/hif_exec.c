arter97-mirror/caf_qca-wifi-host-cmn

qcacmn: Fix issue of napi_struct being deleted multiple times
During WLAN idle shutdown, hif_exec_napi_kill() may be invoked
multiple times, resulting in repeated calls to netif_napi_del()
on the same napi_struct. This can corrupt the kernel's napi_hash
array and lead to a panic when accessing it.

To prevent this, netif_napi_del() is now executed only when
the 'inited' flag of hif_exec_context is true, ensuring it pairs
correctly with netif_napi_add().

Change-Id: I8de6ee0ad6c0687536ee6c58170be88d54913efa
CRs-Fixed: 4288968

hif/src/hif_exec.c
@@ -985,6 +985,7 @@ static void hif_exec_napi_kill(struct hif_exec_context *ctx)
 
 	if (ctx->inited) {
 		qdf_napi_disable(&n_ctx->napi);
+		qdf_netif_napi_del(&n_ctx->napi);
 		ctx->inited = 0;
 	}
 
@@ -992,7 +993,6 @@ static void hif_exec_napi_kill(struct hif_exec_context *ctx)
 		hif_irq_affinity_remove(ctx->os_irq[irq_ind]);
 
 	hif_core_ctl_set_boost(false);
-	qdf_netif_napi_del(&(n_ctx->napi));
 	qdf_net_if_destroy_dummy_if((struct qdf_net_if *)dummy_nd);
 	qdf_napi_set_dummy_nd_ptr(n_ctx, NULL);
 }

Guxin12/android_kernel_xiaomi_marble

hif/src/hif_exec.c
@@ -725,6 +725,7 @@ static void hif_exec_napi_kill(struct hif_exec_context *ctx)
 
 	if (ctx->inited) {
 		napi_disable(&n_ctx->napi);
+		netif_napi_del(&n_ctx->napi);
 		ctx->inited = 0;
 	}
 
@@ -732,7 +733,6 @@ static void hif_exec_napi_kill(struct hif_exec_context *ctx)
 		hif_irq_affinity_remove(ctx->os_irq[irq_ind]);
 
 	hif_core_ctl_set_boost(false);
-	netif_napi_del(&(n_ctx->napi));
 	qdf_net_if_destroy_dummy_if((struct qdf_net_if *)dummy_nd);
 	qdf_napi_set_dummy_nd_ptr(n_ctx, NULL);
 }