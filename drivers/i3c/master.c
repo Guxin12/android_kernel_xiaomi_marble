47b5967f985471ee347b77235a7e6b5a21877e92
Pzqqt <821026875@qq.com>
2026-01-10 17:04:28 +0800

    Revert "i3c: Allow OF-alias-based persistent bus numbering"
    This reverts commit c2c2bbd9908884407cd2af238248575fabd46f68.

--- drivers/i3c/master.c
+++ drivers/i3c/master.c
@@ -21,7 +21,6 @@
 
 static DEFINE_IDR(i3c_bus_idr);
 static DEFINE_MUTEX(i3c_core_lock);
-static int __i3c_first_dynamic_bus_num;
 
 /**
  * i3c_bus_maintenance_lock - Lock the bus for a maintenance operation
@@ -424,9 +423,9 @@ static void i3c_bus_cleanup(struct i3c_bus *i3cbus)
 	mutex_unlock(&i3c_core_lock);
 }
 
-static int i3c_bus_init(struct i3c_bus *i3cbus, struct device_node *np)
+static int i3c_bus_init(struct i3c_bus *i3cbus)
 {
-	int ret, start, end, id = -1;
+	int ret;
 
 	init_rwsem(&i3cbus->lock);
 	INIT_LIST_HEAD(&i3cbus->devs.i2c);
@@ -434,19 +433,8 @@ static int i3c_bus_init(struct i3c_bus *i3cbus, struct device_node *np)
 	i3c_bus_init_addrslots(i3cbus);
 	i3cbus->mode = I3C_BUS_MODE_PURE;
 
-	if (np)
-		id = of_alias_get_id(np, "i3c");
-
 	mutex_lock(&i3c_core_lock);
-	if (id >= 0) {
-		start = id;
-		end = start + 1;
-	} else {
-		start = __i3c_first_dynamic_bus_num;
-		end = 0;
-	}
-
-	ret = idr_alloc(&i3c_bus_idr, i3cbus, start, end, GFP_KERNEL);
+	ret = idr_alloc(&i3c_bus_idr, i3cbus, 0, 0, GFP_KERNEL);
 	mutex_unlock(&i3c_core_lock);
 
 	if (ret < 0)
@@ -2622,7 +2610,7 @@ int i3c_master_register(struct i3c_master_controller *master,
 	INIT_LIST_HEAD(&master->boardinfo.i2c);
 	INIT_LIST_HEAD(&master->boardinfo.i3c);
 
-	ret = i3c_bus_init(i3cbus, master->dev.of_node);
+	ret = i3c_bus_init(i3cbus);
 	if (ret)
 		return ret;
 
@@ -2831,16 +2819,8 @@ void i3c_dev_free_ibi_locked(struct i3c_dev_desc *dev)
 
 static int __init i3c_init(void)
 {
-	int res;
+	int res = bus_register_notifier(&i2c_bus_type, &i2cdev_notifier);
 
-	res = of_alias_get_highest_id("i3c");
-	if (res >= 0) {
-		mutex_lock(&i3c_core_lock);
-		__i3c_first_dynamic_bus_num = res + 1;
-		mutex_unlock(&i3c_core_lock);
-	}
-
-	res = bus_register_notifier(&i2c_bus_type, &i2cdev_notifier);
 	if (res)
 		return res;
 
