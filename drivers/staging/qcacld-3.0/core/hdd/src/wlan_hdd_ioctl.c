68b0246f74a74a7c8f63c39a317076e65a6a5e56
Pzqqt <821026875@qq.com>
2026-04-13 10:14:14 +0800

    Revert "qcacld-3.0: Import Xiaomi changes"
    AOSP roms may not need these changes.
    
    This reverts commit 58862db140d03c41314a671d1023412716019119.
    
    Conflicts:
    	drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_apf.c
    	drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_ioctl.c

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_ioctl.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_ioctl.c
@@ -2692,7 +2692,6 @@ static int drv_cmd_get_country(struct hdd_adapter *adapter,
 	return ret;
 }
 
-
 static int drv_cmd_set_roam_trigger(struct hdd_adapter *adapter,
 				    struct hdd_context *hdd_ctx,
 				    uint8_t *command,
@@ -3091,7 +3090,7 @@ static int drv_cmd_set_suspend_mode(struct hdd_adapter *adapter,
 		return -EINVAL;
 	}
 
-	hdd_debug("APF status and idle_monitor:%d, ", idle_monitor);
+	hdd_debug("idle_monitor:%d", idle_monitor);
 	if (idle_monitor == 0)
 		hdd_disable_active_apf_mode(adapter, hdd_ctx);
 	else if (idle_monitor == 1)
@@ -5195,41 +5194,6 @@ static int drv_cmd_set_app2_params(struct hdd_adapter *adapter,
 }
 #endif /* WLAN_FEATURE_EXTWOW_SUPPORT */
 
-#ifdef CFG_SUPPORT_SCAN_EXT_FLAG
-/*
- * argv: 1 means that force scan on gaming mode
- */
-static int driver_cmd_set_scan_ext_flag(struct hdd_adapter *adapter,
-				   struct hdd_context *hdd_ctx,
-				   uint8_t *command,
-				   uint8_t command_len,
-				   struct hdd_priv_data *priv_data)
-{
-	int ret = 0;
-	uint8_t *value = command;
-	uint8_t external_flag = 0;
-
-	/* Move pointer to ahead of SetScanExtFlag */
-	value = value + command_len + 1;
-
-	/* Convert the value from ascii to integer */
-	ret = kstrtou8(value, 10, &external_flag);
-	if (ret < 0) {
-		/*
-		 * If the input value is greater than max value of datatype,
-		 * then also kstrtou8 fails
-		 */
-		hdd_err("kstrtou8 failed Input value may be out of range");
-		ret = -EINVAL;
-		return ret;
-	}
-
-	adapter->scan_ext_flag = external_flag;
-	hdd_debug("driver_cmd_set_scan_ext_flag: %d ", adapter->scan_ext_flag);
-	return ret;
-}
-#endif /* CFG_SUPPORT_SCAN_EXT_FLAG */
-
 #ifdef FEATURE_WLAN_TDLS
 /**
  * drv_cmd_tdls_secondary_channel_offset() - secondary tdls off channel offset
@@ -6690,36 +6654,6 @@ static int drv_cmd_get_disable_chan_list(struct hdd_adapter *adapter,
 }
 #endif
 
-static int drv_cmd_set_phymode(struct hdd_adapter *adapter,
-					struct hdd_context *hdd_ctx,
-					uint8_t *command,
-					uint8_t command_len,
-					struct hdd_priv_data *priv_data)
-{
-	int ret = 0;
-	uint8_t *value = command;
-	uint8_t new_phymode = 0;
-
-	/* Move pointer to ahead of SET_PHYMODE<delimiter> */
-	value = value + command_len + 1;
-
-	/* Convert the value from ascii to integer */
-	ret = kstrtou8(value, 10, &new_phymode);
-	if (ret < 0) {
-		/*
-		 * If the input value is greater than max value of datatype,
-		 * then also kstrtou8 fails
-		 */
-		hdd_err("kstrtou8 failed Input value may be out of range");
-		ret = -EINVAL;
-		goto exit;
-	}
-
-	hdd_we_update_phymode(adapter, new_phymode);
-exit:
-	return ret;
-}
-
 #ifdef FEATURE_ANI_LEVEL_REQUEST
 static int drv_cmd_get_ani_level(struct hdd_adapter *adapter,
 				 struct hdd_context *hdd_ctx,
@@ -7063,15 +6997,11 @@ static const struct hdd_drv_cmd hdd_drv_cmds[] = {
 	{"GET_FUNCTION_CALL_MAP",     drv_cmd_get_function_call_map, true},
 #endif
 	{"STOP",                      drv_cmd_dummy, false},
-	{"SET_PHYMODE",               drv_cmd_set_phymode, true},
 	/* Deprecated commands */
 	{"RXFILTER-START",            drv_cmd_dummy, false},
 	{"RXFILTER-STOP",             drv_cmd_dummy, false},
 	{"BTCOEXSCAN-START",          drv_cmd_dummy, false},
 	{"BTCOEXSCAN-STOP",           drv_cmd_dummy, false},
-#ifdef CFG_SUPPORT_SCAN_EXT_FLAG
-	{"SetScanExtFlag",            driver_cmd_set_scan_ext_flag, true},
-#endif
 };
 
 /**
